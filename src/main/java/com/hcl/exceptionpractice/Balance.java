682e363584e452dfd6acee993e1d3d76662f53fa
Tin Truong <tintruong@Tins-Air.attlocal.net>
2022-02-03 04:40:42 -0500

    Create Exception class

--- src/main/java/com/hcl/exceptionpractice/Balance.java
+++ src/main/java/com/hcl/exceptionpractice/Balance.java
@@ -1,5 +1,39 @@
 package com.hcl.exceptionpractice;
 
+import java.util.Scanner;
+
 public class Balance {
+	private Double total
+	this.total = 0;
+			
+
+
+public void deposit(double amount) {
+	this.total += amount;
+}
+
+public void withdraw(double amount) throws InsufficientException {
+	if(this.total < amount) {
+		throw new InsufficientException("Insufficient Amount");
+	}
+	this.total -= amount;
+}
+	
+
+public void withdraw(String[] args) {
+	Scanner w = new scanner (System.in);
+	double b = new Balance();
+	System.out.println("Enter amount to withdrawal");
+	double w = w.nextDouble();
+	try {
+		if (total < w) {
+			throw InsufficientException
+			} else {
+				System.out.println(w)
+				}
+		
+	} catch(InsufficientException e) {
+		System.out.println("Insufficient Amount: " + e); }
+	}
 
 }
---
+++ src/main/java/com/hcl/exceptionpractice/InsufficientException.java
@@ -0,0 +1,19 @@
+package com.hcl.exceptionpractice;
+
+import java.util.Scanner;
+
+public class InsufficientException extends Exception {
+	
+	String msg;
+	
+	public InsufficientException(String msg)
+	this.msg = msg;
+	
+	public String toString() {
+		return msg;
+	}
+	
+	
+	
+
+}
