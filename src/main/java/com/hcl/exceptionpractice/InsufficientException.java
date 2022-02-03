05a6ec8fc3382482508d77afd0d87e2ecef7ac88
Tin Truong <tintruong@Tins-Air.attlocal.net>
2022-02-03 11:36:56 -0500

    Modified file

--- src/main/java/com/hcl/exceptionpractice/InsufficientException.java
+++ src/main/java/com/hcl/exceptionpractice/InsufficientException.java
@@ -6,9 +6,9 @@ public class InsufficientException extends Exception {
 	
 	String msg;
 	
-	public InsufficientException(String msg)
+	public InsufficientException(String msg
 	this.msg = msg;
-	
+}
 	public String toString() {
 		return msg;
 	}
--- src/main/java/com/hcl/stringpractice/Name.java
+++ src/main/java/com/hcl/stringpractice/Name.java
@@ -1,5 +1,28 @@
 package com.hcl.stringpractice;
 
+import java.util.StringTokenizer;
+
 public class Name {
+	
+	public static void main(String[] args) {
+		
+	String str= "Tin_Trung_Truong";
+	StringTokenizer st = new StringTokenizer(str);
+	
+	String str2 = st.nextToken("_")+ " ";
+	
+	while(st.hasMoreTokens()) {
+		str2 += (st.nextToken("_") + " ");
+	}
+	System.out.println(str2);
+	
+	StringBuffer strB = new StringBuffer();
+	strB.append(str2);
+	strB.append("11/24/1997");
+	str2 = strB.toString();
+	
+	System.out.println(str2);
+			
+	}
 
 }
